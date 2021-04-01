82345310598060290c59fa7728436e6b60a97003
Monika Nogała <mxnogala@gmail.com>
2021-04-01 11:32:57 +0200

    mobile apps- lab 2

---
+++ Mobile_Applications/Phoneword/Phoneword/Phoneword/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+namespace Phoneword
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        string translatedNumber;
+        public MainPage()
+        {
+            InitializeComponent();   
+
+        }
+
+        void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = phoneNumber.Text;
+            translatedNumber = PhonewordTranslator.ToNumber(enteredNumber);
+
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
+        }
+
+        async void OnCall(object sender, System.EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                "Dial a Number",
+                "Would you like to call" + translatedNumber + "?",
+                "Yes",
+                "No"))
+            {
+                try
+                {
+                    PhoneDialer.Open(translatedNumber);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone was not valid", "Ok");
+                }
+                catch (FeatureNotEnabledException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone dialing not supported", "Ok");
+                }
+                catch (Exception)
+                {
+                    await DisplayAlert("Unable to dial", "Phone dialig failed", "Ok");
+                }
+            }
+        }
+
+    }
+}
